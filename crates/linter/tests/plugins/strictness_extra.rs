3dcc8202d739779586be92f685a8e36b0c9a2092
vvvince <vvvince@ofcompute.rs>
2025-02-22 17:22:27 +0100

    feat(linter): add strictness-extra/no-empty-construct rule

--- crates/linter/tests/plugins/strictness_extra.rs
+++ crates/linter/tests/plugins/strictness_extra.rs
@@ -1,5 +1,7 @@
+use mago_linter::plugin::strictness_extra::rules::no_empty_construct::NoEmptyConstruct;
 use mago_linter::plugin::strictness_extra::rules::require_strict_behavior::RequireStrictBehavior;
 
 use crate::rule_test;
 
+rule_test!(test_no_empty, NoEmptyConstruct);
 rule_test!(test_require_strict_behavior, RequireStrictBehavior);
