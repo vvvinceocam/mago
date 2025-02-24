b32a4f439f486eec4dd29a0a231e6da173c965ae
vvvince <vvvince@ofcompute.rs>
2025-02-24 09:04:29 +0100

    refactor(linter): move strictness-extra rules into strictness plugin

--- crates/linter/tests/plugins/strictness_extra.rs
+++
@@ -1,9 +0,0 @@
-use mago_linter::plugin::strictness_extra::rules::no_elvis_operator::NoElvisOperator;
-use mago_linter::plugin::strictness_extra::rules::no_empty_construct::NoEmptyConstruct;
-use mago_linter::plugin::strictness_extra::rules::require_strict_behavior::RequireStrictBehavior;
-
-use crate::rule_test;
-
-rule_test!(test_no_elvis_operator, NoElvisOperator);
-rule_test!(test_no_empty_construct, NoEmptyConstruct);
-rule_test!(test_require_strict_behavior, RequireStrictBehavior);
